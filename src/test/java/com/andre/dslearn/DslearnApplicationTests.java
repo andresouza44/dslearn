b1a986800536294989a7c281620f1ca23c3871f2
Andre M. de Souza <andresouza44@gmail.com>
2024-06-16 17:30:41 -0300

    iniciando projeto

---
+++ src/test/java/com/andre/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
